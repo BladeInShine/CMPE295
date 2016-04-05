c9cf2b71d2fa2c9ddd6e1f39bd1a155a1a05c48b
Sammie <sammiexiu@gmail.com>
2016-04-04 23:57:17 -0700

    upload image failed, plz help

--- src/main/webapp/scripts/app/entities/history/history-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/history/history-dialog.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('cmpe295App').controller('HistoryDialogController',
-    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'History', 'User',
-        function($scope, $stateParams, $uibModalInstance, entity, History, User) {
+    ['$scope', '$http', '$stateParams', '$uibModalInstance', 'entity', 'History', 'User',
+        function($scope, $http, $stateParams, $uibModalInstance, entity, History, User) {
 
         $scope.history = entity;
         $scope.users = User.query();
@@ -42,5 +42,28 @@ angular.module('cmpe295App').controller('HistoryDialogController',
 
         $scope.datePickerForTimeOpen = function($event) {
             $scope.datePickerForTime.status.opened = true;
+        };
+
+        $scope.uploadFile = function(files) {
+            var uploadUrl = 'http://127.0.0.1:8080/api/historys/photo'
+            var f = files[0], r = new FileReader();
+            r.onloadend = function(e) {
+                var data = e.target.result;
+                var fd = new FormData();
+                fd.append('file', data);
+                fd.append('file_name', f.name);
+                $http.post(uploadUrl, fd, {
+                    withCredentials: true,
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                })
+                .success(function(){
+                    console.log('success');
+                })
+                .error(function(){
+                    console.log('error');
+                });
+            };
+            r.readAsDataURL(f);
         };
 }]);
