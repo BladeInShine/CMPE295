0427986de27b5e420e86d215098582ee39931351
angelqian <angelqian2@gmail.com>
2016-04-17 22:40:58 -0700

    fix bug in loginview

--- src/main/webapp/scripts/app/main/main.js
+++ src/main/webapp/scripts/app/main/main.js
@@ -12,7 +12,7 @@ angular.module('cmpe295App')
                 views: {
                     'content@': {
                         templateUrl: 'scripts/app/main/main.html',
-                        controller: 'MainController'
+                        controller: ['MainController','LoginController']
                     }
                 },
                 resolve: {
