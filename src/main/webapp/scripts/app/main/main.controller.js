9b820fb348cc04fdad48ae5ab15365f7dd7e2e24
Sammie <sammiexiu@gmail.com>
2016-05-03 00:43:44 -0700

    call rest api to populate history data to table

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,12 +1,26 @@
 'use strict';
 
 angular.module('cmpe295App')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, $http) {
         Principal.identity().then(function(account) {
             $scope.$parent.usingRecommender = account!=null;
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
             console.log("12======="+ $scope.$parent.usingRecommender);
+        });
+        $http.get('/api/historys/table', {
+            withCredentials: true,
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(response){
+            console.log(response);
+            for (var i = 0; i < response.length; i++) {
+                drawRow(response[i]);
+            }
+        })
+        .error(function(response){
+            console.log('error');
         });
 
     });
