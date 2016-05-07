98d8b80c84659a5e2ab0ab338a0fdb11a3660914
angelqian <angelqian2@gmail.com>
2016-05-07 15:03:16 -0700

    update star rating logic

--- src/main/webapp/scripts/app/entities/recHistory/recHistory.controller.js
+++ src/main/webapp/scripts/app/entities/recHistory/recHistory.controller.js
@@ -2,6 +2,10 @@
 
 angular.module('cmpe295App')
     .controller('RecHistoryController', function ($scope, $state, RecHistory) {
+        $scope.rating = 5;
+        $scope.saveRatingToServer = function(rating) {
+            console.log('Rating selected - ' + rating);
+        };
 
         $scope.recHistorys = [];
         $scope.loadAll = function() {
@@ -27,4 +31,44 @@ angular.module('cmpe295App')
                 id: null
             };
         };
-    });
+    })
+    .directive('fundooRating', function () {
+        return {
+            restrict: 'A',
+            template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+            '\u2605' +
+            '</li>' +
+            '</ul>',
+            scope: {
+                ratingValue: '=',
+                max: '=',
+                readonly: '@',
+                onRatingSelected: '&'
+            },
+            link: function (scope, elem, attrs) {
+
+                var updateStars = function() {
+                    scope.stars = [];
+                    for (var  i = 0; i < scope.max; i++) {
+                        scope.stars.push({filled: i < scope.ratingValue});
+                    }
+                };
+
+                scope.toggle = function(index) {
+                    if (scope.readonly && scope.readonly === 'true') {
+                        return;
+                    }
+                    scope.ratingValue = index + 1;
+                    scope.onRatingSelected({rating: index + 1});
+                };
+
+                scope.$watch('ratingValue', function(oldVal, newVal) {
+                    if (newVal) {
+                        updateStars();
+                    }
+                });
+            }
+        }
+    })
+;
